246f3b566a0b90af3cbdaccf990e595cc73ef45a
Paula Ponce <paula.poncez@usach.cl>
2017-05-18 23:59:46 -0400

    forEach para mostrar imágenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var contenedorImgs = document.getElementsByClassName("cuadros")[0];
+
+pictures.forEach(function(element){
+	var img = document.createElement("img");
+	img.setAttribute("src", element);
+	img.setAttribute("class", "panda-cuadros");
+	contenedorImgs.appendChild(img);
+})
